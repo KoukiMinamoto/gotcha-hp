39c14c187d799cdea3ebe5e5877451f3a652508c
KoukiMinamoto <tomatomusicstudio@gmail.com>
2021-08-25 02:28:17 +0900

    Completed HeadCarousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import HeadCarousel from 'HeadCarousel';
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App: FC = () => (
-  <body>
-    <HeadCarousel />
-  </body>
-);
+const scrollAmount = (): number =>
+  Math.max(
+    window.pageYOffset,
+    document.documentElement.scrollTop,
+    document.body.scrollTop,
+  );
+
+const App: FC = () => {
+  // States
+  const [isScrollUp, setScrollUp] = useState(false);
+
+  const detectScrollUp = (): void => {
+    if (scrollAmount() > 10) {
+      setScrollUp(true);
+    } else {
+      setScrollUp(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', detectScrollUp);
+
+    return (): void => document.removeEventListener('scroll', detectScrollUp);
+  });
+
+  return (
+    <>
+      <div className="back-ground"> </div>
+      <HeadCarousel disapear={isScrollUp} />
+    </>
+  );
+};
 
 export default App;
--- src/HeadCarousel.tsx
+++ src/HeadCarousel.tsx
@@ -1,10 +1,12 @@
 import { VFC } from 'react';
+
 import headerLogo from './svg/header_logo.svg';
+import style from './styles/HeadCarousel.module.scss';
 // import img1 from './img/head1.jpg';
 // import img2 from './img/head2.jpg';
 // import img3 from './img/head3.jpg';
 
-const HeadCarousel: VFC = () => (
+const HeadCarousel: VFC<{ disapear: boolean }> = (props) => {
   // const [_index, setIndex] = useState(0);
 
   // const handleSelect = (
@@ -13,13 +15,19 @@ const HeadCarousel: VFC = () => (
   // ) => {
   //   setIndex(selectedIndex);
   // };
-  <>
-    <div className="carousel">
-      <img className="header-logo" src={headerLogo} alt="header_logo" />
-      <div className="box1" />
-      <div className="scroll-up"> Scroll Up </div>
-    </div>
-  </>
-);
+  const { disapear } = props;
+
+  return (
+    <>
+      <div className={disapear ? style['carousel-disapear'] : style.carousel}>
+        <img className={style.headerLogo} src={headerLogo} alt="header_logo" />
+        <div className={style.scrollIcon}>
+          <div className={style.box1} />
+          <div className={style['scroll-up']}> Scroll Up </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HeadCarousel;
